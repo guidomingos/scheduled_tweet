4e4a87c13431daeeff76760ad174a6a8049cdc77
Guilherme <guilherme63@gmail.com>
2023-10-02 20:00:49 -0300

    list of users / delete & update users

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_14_125923) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_30_031005) do
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
   end
 
 end
